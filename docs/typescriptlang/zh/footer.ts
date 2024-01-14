2aa7e6ca9faa0d1273e6884b030ef31b58729419
Enoch Gao <enochgao@qq.com>
2024-01-14 19:57:02 +0800

    Update docs/typescriptlang/zh/footer.ts
    Co-authored-by: Xavi Lee <xavilee2002@outlook.com>

--- docs/typescriptlang/zh/footer.ts
+++ docs/typescriptlang/zh/footer.ts
@@ -1,6 +1,6 @@
 export const footerCopy = {
   footer_customize: '自定义',
-  footer_site_colours: '站点颜色',
+  footer_site_colours: '主题模式',
   footer_code_font: '代码字体',
   footer_site_colours_options_system: '系统',
   footer_site_colours_options_always_light: '明亮',
