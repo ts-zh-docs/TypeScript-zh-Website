be6f82ad11d9c2ab35025f1c77650802e6c9297e
Xavi Lee <xavilee2002@outlook.com>
2024-01-14 20:00:31 +0800

    Apply suggestions from code review

--- docs/typescriptlang/zh/footer.ts
+++ docs/typescriptlang/zh/footer.ts
@@ -2,7 +2,7 @@ export const footerCopy = {
   footer_customize: '自定义',
   footer_site_colours: '主题模式',
   footer_code_font: '代码字体',
-  footer_site_colours_options_system: '系统',
-  footer_site_colours_options_always_light: '明亮',
+  footer_site_colours_options_system: '跟随系统',
+  footer_site_colours_options_always_light: '浅色模式',
   footer_site_colours_options_always_dark: '深色模式',
 };
